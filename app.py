f14fb89fc448942fec928406ddfe74fe5f1ab330
yashkumary385 <114864924+yashkumary385@users.noreply.github.com>
2025-10-02 21:57:13 +0530

    completed sentiment analyzer

---
+++ app.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+# Load pipeline
+pipeline = joblib.load("sentiment_pipeline.pkl")
+
+app = FastAPI()
+
+# Pydantic model
+class TextInput(BaseModel):
+    text: str
+
+# Simple home route
+@app.get("/")
+def home():
+    return {"message": "API is working!"}
+
+# Prediction route
+@app.post("/predict")
+def predict(input_data: TextInput):
+    # Get prediction
+    prediction = pipeline.predict([input_data.text])[0]
+
+    # Ensure it's JSON-serializable
+    if isinstance(prediction, (int, float, str)): # if any of these 
+        if (prediction == 1):
+           return {"sentiment Negative":  str(prediction)}
+        else:
+            return {"sentiment Positive":  str(prediction)}
+    else:
+          if (prediction == 1):
+           return {"sentiment":  f" Negative {str(prediction)} "}
+          else:
+            return {"sentiment": f" Positive {str(prediction)}"}
+        # return {"sentiment": str(prediction)}
